6afa37b97f8766a3ab7c4d86ffa046c340df5e8f
Rajat Kamboj <142865956+rajatkamboj06@users.noreply.github.com>
2025-01-13 11:14:00 +0530

    Create Lab1_q3.py

---
+++ Lab1_q3.py
@@ -0,0 +1,11 @@
+name = input("Enter your name here: ")
+
+greet = f"Hello, {name}! Nice to meet you!!"
+
+dob = int(input("Enter your year of birth: "))
+
+current_year = 2024
+age = current_year - dob
+
+print(greet)
+print(f"Currently, your age is: {age}")
