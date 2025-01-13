48c9f7b36e0d96c73353a0193336580e93e47c1b
Rajat Kamboj <142865956+rajatkamboj06@users.noreply.github.com>
2025-01-13 11:20:53 +0530

    Update Lab1_q4.py

--- Lab1_q4.py
+++ Lab1_q4.py
@@ -1,7 +1,5 @@
-# Prompt the user to enter a number
 number = int(input("Enter the number you want to get checked: "))
 
-# Use the modulus operator to check if the number is even or odd
 if (number % 2) == 0:
     print("The number given is even.")
 else:
